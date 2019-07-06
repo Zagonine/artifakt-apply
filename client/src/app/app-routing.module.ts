c829ce8fb43110e2cf01f9b4e76899e322501d68
Zagonine <mail@enzogain.com>
2019-07-07 01:03:00 +0200

    Creation of projects list page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ProjectsListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/components/projects-list/projects-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-projects-list',
+  templateUrl: './projects-list.component.html',
+  styleUrls: ['./projects-list.component.scss']
+})
+export class ProjectsListComponent implements OnInit {
+
+  constructor(private apiService: ApiService) {}
+
+  projectsList = []
+  error = null
+
+  ngOnInit() {
+    this.apiService.getProjectsList().subscribe(
+      (data: any) => this.projectsList = data.projects,
+      error => {
+        this.error = error.error.msg || error.message || 'Failed retrieve projects'
+      }
+    )
+  }
+
+}
